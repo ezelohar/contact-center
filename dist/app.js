3b5b183a65cbb4a805fcb763e7dc2cb4f661269f
Zoran Lazic <ezelohar@gmail.com>
2015-06-18 17:00:30 +0200

    add final changes

--- dist/app.js
+++ dist/app.js
@@ -383,8 +383,6 @@ angular.module('ContactCenter').factory('dataManager', ['$q', '$http', '_', 'mom
 			store._saveToLocalStorage(store.callsStoreID, data);
 			angular.copy(data, store.calls);
 
-
-			console.log(store.calls);
 			return store.calls;
 		},
 
@@ -414,7 +412,6 @@ angular.module('ContactCenter').factory('dataManager', ['$q', '$http', '_', 'mom
 					deferred.reject(data);
 				}
 			}).error(function (data, status) {
-				console.log(osa);
 				deferred.reject(data);
 			});
 
